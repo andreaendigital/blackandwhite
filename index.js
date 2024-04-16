6c5dd46be79c0a17e3121f07c4f773e81f632703
Andrea Rosero <andrea.rosero.p@gmail.com>
2024-04-16 13:53:28 -0400

    ordenando código y agregando documentación, creación de carpeta assets y archivo css

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-// importando los modulos requeridos
+// Instalo en terminal:
+// npm init --yes
+// npm install express
+// npm i nodemon --D
+// npm i jimp
+// npm i uuid
+// npm install --save bootstrap
+
+// creo un servidor con express e importo los módulos requeridos
 const express = require("express");
-
-const app = express();
-
 const Jimp = require('jimp');
-
 const { v4: uuidv4 } = require("uuid");
-
+const fs = require('fs');
+const path = require('path');
 const _ = require("lodash");
 
-const fs = require('fs');
-
-const path = require('path');
-
-
-
+const app = express(); //instancio express
 
 // Iniciar el servidor
 const PORT = process.env.PORT || 3000;
@@ -22,18 +22,25 @@ app.listen(PORT, () => {
     console.log("Servidor Express iniciado en el puerto" + PORT);
 });
 
+// Defino la ruta a carpeta pública de los archivos para llamar al css
+app.use(express.static(__dirname + "./assets/css"));
 
+//crear un middleware que define una ruta /bootstrap y libere el contenido de la carpeta "css", 
+//dependencia de bootstrap en el node_module previamente instalada con nmp
+app.use('/bootstrap',
+    express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
+//crear un middleware que define una ruta /bootstrap y libere el contenido de la carpeta "js", 
+//dependencia de bootstrap en el node_module previamente instalada con nmp
+app.use('/bootstrapjs',
+    express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 
 // Ruta raíz para mostrar el formulario HTML
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-
-
 // codigo para leer: 
-// Paso 4
 // app.get("/leer", (req, res) => {
 //     // Paso 5
 //     const { url } = req.query
@@ -81,21 +88,27 @@ app.get('/cargar', async (req, res) => {
         } else {
             throw new Error('Debe proporcionar una URL o una ruta de archivo.');
         }
+
+        //asigna un nombre definido por los primeros seis caracteres de UUID, con extensión.jpeg capturandolo en la variable "newname"
         const newname = uuidv4().slice(0, 6) + '.jpeg';
 
         await imagen
-            .resize(350, 350) // resize
+            .resize(350, Jimp.AUTO) // resize
             .quality(100) // set JPEG quality
             .greyscale() // set greyscale
-            .writeAsync(newname); // save
+            .writeAsync(newname); // guarda la imagen con el nuevo nombre definido previamente
+        res.sendFile(__dirname + '/' + newname); //devuelve el archivo procesado al cliente, con el nuevo nombre asignado
 
-        res.sendFile(__dirname + '/' + newname);
     } catch (error) {
         // Manejar errores
         console.error('Error al procesar la imagen:', error.message);
+        // Devuelve una respuesta de error al cliente
         res.status(500).send('Error interno del servidor');
+        
     }
 });
+
+
 
 // app.get('/cargar', async (req, res) => {
 //     const { url } = req.query
@@ -121,3 +134,14 @@ app.get('/cargar', async (req, res) => {
 //     res.status(500).send('Error interno del servidor');
 // };
 // });
+
+// renombrar: 
+// const{ nombre, nuevo } = req.query
+//console.log("Valor de parametro nombre: ", nombre)
+//console.log("Valor de parametro nombre: ", nuevo)
+
+
+// Paso 3
+// fs.rename(nombre, nuevo, () => {
+//     res.status(200).send(`Archivo ${nombre} fue renombrado como ${nuevo}`)
+// })
