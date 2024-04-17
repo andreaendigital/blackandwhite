83e0f0c64a995a0c2aaf3f7db364d01bee618871
Andrea Rosero <andrea.rosero.p@gmail.com>
2024-04-17 19:58:31 -0400

    mejoras en documentación, manejo de errores, y revision de archivos en directorio raiz

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const Jimp = require('jimp');
 const { v4: uuidv4 } = require("uuid");
 const fs = require('fs');
 const path = require('path');  //creo que debemos eliminarlo
-// const _ = require("lodash");
 
 const app = express(); //instancio express
 
@@ -22,10 +21,11 @@ app.listen(PORT, () => {
     console.log("Servidor Express iniciado en el puerto" + PORT);
 });
 
-// Defino la ruta a carpeta pública de los archivos para llamar al css
+// Se configura carpeta public como publica, según lo pedido en requerimientos
+app.use(express.static(__dirname + "/public"));
+
+// Se define la ruta a carpeta pública de los archivos
 app.use(express.static(__dirname + "/assets"));
-// app.use(express.static(__dirname + "/assets/img"));
-// app.use(express.static('assets'));
 
 //crear un middleware que define una ruta /bootstrap y libere el contenido de la carpeta "css", 
 //dependencia de bootstrap en el node_module previamente instalada con nmp
@@ -37,10 +37,6 @@ app.use('/bootstrap',
 app.use('/bootstrapjs',
     express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 
-//configuro carpeta public como publica
-
-app.use(express.static(__dirname + "/public"));   
-
 // Ruta raíz para mostrar el formulario HTML
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html");
@@ -48,24 +44,24 @@ app.get('/', (req, res) => {
 
 app.get('/cargar', async (req, res) => {
     const { url } = req.query; //destructuring, obtengo el parámetro que necesito
-    const {archivo} = req.query; //destructuring, obtengo el parámetro que necesito
+    const { archivo } = req.query; //destructuring, obtengo el parámetro que necesito
     try {
         // const imagen = await Jimp.read(url) || await Jimp.read(archivo);
+  
         if (url == '') { //si en el input se introduce un string vacío
             throw new Error('Debe proporcionar una URL válida.'); //mensaje a mostrar
         }
         if (archivo == '') { //si la variable está vacía, null o undefined
             throw new Error('Debe proporcionar una ruta de archivo.'); //mensaje a mostrar
         }
+      
         if (url) {
             // Si se proporciona una URL, carga la imagen desde la URL
             imagen = await Jimp.read(url);
         } else {
-            console.log('estoy aqui', archivo);
-    
             // Si se proporciona una ruta de archivo, carga la imagen desde el archivo
             imagen = await Jimp.read(archivo);
-        } 
+        }
 
         //asigna un nombre definido por los primeros seis caracteres de UUID, con extensión.jpeg capturandolo en la variable "newname"
         const newname = uuidv4().slice(0, 6) + '.jpeg';
@@ -75,15 +71,16 @@ app.get('/cargar', async (req, res) => {
             .quality(100) // se determina la calidad
             .greyscale() // se procesa a escala de grises/blanco y negro
             .writeAsync(newname); // guarda la imagen con el nuevo nombre definido previamente
-        res.sendFile(__dirname + '/' + newname); //devuelve el archivo procesado al cliente, con el nuevo nombre asignado
+        res.sendFile(__dirname + '/' + newname); //devuelve el archivo procesado al cliente, y en el directorio raíz se crea con el nuevo nombre asignado
 
     } catch (error) {
-      
+
         // Manejar errores
         console.error('Error al procesar, mensaje del catch:', error.message); //muestra en consola el error que procesa
         console.error('url:' + typeof url, 'archivo: ' + typeof archivo); //muestra en consola el tipo de error
-        // Devuelve una respuesta de error al cliente, según errores definidos en el try
-        res.status(500).send(error.message);
-        
+        // Devuelve una respuesta de error al cliente, según errores definidos en el try:
+        res.status(500).send('Ups! Debe haber algún error, recomendamos revisar bien la ruta. El sistema dice: ' + error.message); // muestra el mensaje personalizado de error de línea 52 y 55
+        // res.status(500).send('Ups! La ruta debe tener algún error...  inténtelo de nuevo');  // no muestra el mensaje de error personalizado de los casos anteriores y en todos los casos sólo se muestra este mensaje
+
     }
 });
